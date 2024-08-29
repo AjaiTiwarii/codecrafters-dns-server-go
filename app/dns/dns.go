bed1d3e898d157b2fc05e30d104db9e5a5714e16
AjaiTiwarii <ajait0209@gmail.com>
2024-08-29 22:17:00 +0530

    parsing the header of message

--- app/dns/dns.go
+++ app/dns/dns.go
@@ -1,18 +1,15 @@
 package dns
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
 
-// Message struct represents a DNS message
 type Message struct {
-	ID       uint16
 	Header   []byte
 	Question []byte
 	Answer   []byte
-	OPCODE   uint16
-	RD       bool
 }
 
-// NewMessage creates a new DNS message with initialized fields
 func NewMessage() *Message {
 	return &Message{
 		Header:   make([]byte, 12),
@@ -21,56 +18,51 @@ func NewMessage() *Message {
 	}
 }
 
-// ParseMessage parses the incoming DNS query packet
-func ParseMessage(buf []byte) *Message {
-	message := NewMessage()
-	message.ID = binary.BigEndian.Uint16(buf[0:2])
-	message.OPCODE = (binary.BigEndian.Uint16(buf[2:4]) & 0x7800) >> 11
-	message.RD = (buf[2] & 0x01) == 1
-	return message
-}
+func (m *Message) SetHeader(id uint16, qr, opcode, rd uint16) {
+	binary.BigEndian.PutUint16(m.Header[0:2], id)
+	var rcode uint16
 
-// SetHeader sets the DNS header fields based on the received message
-func (m *Message) SetHeader(request *Message) {
-	binary.BigEndian.PutUint16(m.Header[0:2], request.ID) // Mimic the received ID
-	m.Header[2] = 1 << 7                                  // QR flag set to 1 (response)
-	m.Header[2] |= uint8(request.OPCODE << 3)             // Mimic OPCODE
-	if request.RD {
-		m.Header[2] |= 1 << 0 // Mimic RD if set
+	// Set RCODE based on OPCODE
+	if opcode == 0 {
+		rcode = 0
+	} else {
+		rcode = 4
 	}
-	m.Header[3] = 0 // RA = 0, Z = 0, RCODE = 0
 
-	// Set QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT
-	binary.BigEndian.PutUint16(m.Header[4:6], 1)
-	binary.BigEndian.PutUint16(m.Header[6:8], 1)
+	binary.BigEndian.PutUint16(m.Header[2:4], combineFlags(qr, opcode, 0, 0, rd, 0, 0, rcode))
+	binary.BigEndian.PutUint16(m.Header[4:6], 1) // Question Count
+	binary.BigEndian.PutUint16(m.Header[6:8], 1) // Answer Count
 	binary.BigEndian.PutUint16(m.Header[8:10], 0)
 	binary.BigEndian.PutUint16(m.Header[10:12], 0)
 }
 
-// SetQuestion sets the DNS question section
 func (m *Message) SetQuestion() {
 	m.Question = []byte("\x0ccodecrafters\x02io\x00")
-	m.Question = binary.BigEndian.AppendUint16(m.Question, 1) // Type A
-	m.Question = binary.BigEndian.AppendUint16(m.Question, 1) // Class IN
+	m.Question = binary.BigEndian.AppendUint16(m.Question, 1)
+	m.Question = binary.BigEndian.AppendUint16(m.Question, 1)
 }
 
-// SetAnswer sets the DNS answer section
 func (m *Message) SetAnswer() {
-	answer := []byte("\x0ccodecrafters\x02io\x00")
-	answer = binary.BigEndian.AppendUint16(answer, 1)   // Type A
-	answer = binary.BigEndian.AppendUint16(answer, 1)   // Class IN
-	answer = binary.BigEndian.AppendUint32(answer, 60)  // TTL
-	answer = binary.BigEndian.AppendUint16(answer, 4)   // RDLENGTH
-	answer = append(answer, []byte{8, 8, 8, 8}...) // RDATA (example IP 8.8.8.8)
+	answer := []byte{}
+	answer = append(answer, []byte("\x0ccodecrafters\x02io\x00")...)
+	answer = binary.BigEndian.AppendUint16(answer, 1)
+	answer = binary.BigEndian.AppendUint16(answer, 1)
+	answer = binary.BigEndian.AppendUint32(answer, 60)
+	answer = binary.BigEndian.AppendUint16(answer, 4)
+	answer = binary.BigEndian.AppendUint32(answer, binary.BigEndian.Uint32([]byte("\x08\x08\x08\x08")))
 
 	m.Answer = answer
 }
 
-// PrepareMessage prepares a DNS response based on the received DNS query
-func PrepareMessage(request *Message) *Message {
+func PrepareMessage(id uint16, qr, opcode, rd uint16) *Message {
 	message := NewMessage()
-	message.SetHeader(request)
+	message.SetHeader(id, qr, opcode, rd)
 	message.SetQuestion()
 	message.SetAnswer()
+
 	return message
+}
+
+func combineFlags(qr, opcode, aa, tc, rd, ra, z, rcode uint16) uint16 {
+	return uint16(qr<<15 | opcode<<11 | aa<<10 | tc<<9 | rd<<8 | ra<<7 | z<<4 | rcode)
 }
--- app/main.go
+++ app/main.go
@@ -4,7 +4,7 @@ import (
 	
 	"fmt"
 	"net"
-
+	"encoding/binary"
 	"github.com/codecrafters-io/dns-server-starter-go/app/dns"
 
 )
@@ -40,25 +40,23 @@ func main() {
 			break
 		}
 	
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	
-	
-		
-		// parsing the received message
-		message := dns.ParseMessage(buf[:size])
+		// Extract header fields from the received DNS packet
+		id := binary.BigEndian.Uint16(buf[0:2])
+		opcode := (buf[2] >> 3) & 0x0F
+		rd := (buf[2] >> 0) & 0x01
 
-		// preparing the response DNS message
-		response := dns.PrepareMessage(message)
+		fmt.Printf("Received %d bytes from %s: ID=%d OPCODE=%d RD=%d\n", size, source, id, opcode, rd)
 
-		// Combine Header and Question sections to create the response
-		finalResponse := []byte{}
-		finalResponse = append(finalResponse, response.Header...)
-		finalResponse = append(finalResponse, response.Question...)
-		finalResponse = append(finalResponse, response.Answer...)
+		// Prepare the DNS response message using the extracted fields
+		message := dns.PrepareMessage(id, 1, uint16(opcode), uint16(rd))
+
+		response := []byte{}
+		response = append(response, message.Header...)
+		response = append(response, message.Question...)
+		response = append(response, message.Answer...)
 
 	
-		_, err = udpConn.WriteToUDP(finalResponse, source)
+		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
